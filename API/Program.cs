e82488116610fbcef21ab952e5ddebf0eea41cfb
ugursinans <s.ugursinan@gmail.com>
2023-05-11 00:36:25 +0300

    mikroapi den normal apiye güncellendi. Startup.cs oluşturuldu Program.cs içerisindeki dosyalar değiştirildi. Swagger arayüzü eklendi.

--- API/Program.cs
+++ API/Program.cs
@@ -1,33 +1,5 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using MicroAPI;
+using Microsoft.AspNetCore;
 
-// Add services to the container.
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); }).Build().Run();
